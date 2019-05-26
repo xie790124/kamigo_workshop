d1a99a32f6201920dade212ca05fa01beb43aec8
xie <xoby12@gmail.com>
2019-05-26 16:07:58 +0800

    測試時間上傳

--- app/processors/weather_info.rb
+++ app/processors/weather_info.rb
@@ -7,6 +7,7 @@
         舒適度：#{weather[:ci]}
         溫度：攝氏 #{weather[:min_t]} ~ #{weather[:max_t]} 度
         降雨機率：#{weather[:pop]}%
+        降雨機率：#{weather[:pop]}%
       "
       return {
         "type": "text",
